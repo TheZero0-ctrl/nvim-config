540bc313521f58fbd51bf9862c1043058ba9ac37
TheZero0-ctrl <ankitpariyar486@gmail.com>
2024-12-14 19:34:19 +0545

    added live grep

--- init.lua
+++ init.lua
@@ -1,1 +1,6 @@
 require("zero")
+
+vim.keymap.set("n", "<space><space>x", "<cmd> source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":.lua<CR>")
+
--- lua/zero/plugins/telescope.lua
+++ lua/zero/plugins/telescope.lua
@@ -1,11 +1,31 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+  },
   config = function()
+    require('telescope').setup(
+      {
+        -- defaults = {
+        --   layout_config = {
+        --     height = 1,
+        --   },
+        -- },
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        },
+        extensions = {
+          fzf = {}
+        }
+      }
+    )
+    -- require('telescope').load_extension('fzf')
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>Ps', function()
       local word = vim.fn.expand("<cWORD>")
       builtin.grep_string({ search =  word })
@@ -14,5 +34,7 @@ return {
       local word = vim.fn.expand("<cword>")
       builtin.grep_string({ search =  word })
     end)
+
+    require("zero.plugins.telescope.multigrep").setup()
   end
 }
